d9583af8bde7e786b16bfe6760e7acbcda716403
k1ll1 <blazehardtoerase@gmail.com>
2021-10-21 13:33:39 +0300

    исправлен базовый шаблон, добавлен конфиг для ENV, настройки дев сервера

---
+++ vue/src/components/UI/RPDInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input class="form-control" @input="updateInput" :value="modelValue">
+</template>
+
+<script>
+export default {
+  name: "RPDInput",
+  props: {
+    modelValue: [String, Number]
+  },
+  methods: {
+    updateInput(event){
+      this.$emit('update:modelValue',event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

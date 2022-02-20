9f2c569fd8d21231c2ed59e254a2740363339a7f
k1ll1 <blazehardtoerase@gmail.com>
2022-02-21 01:33:13 +0300

    запилена работа С БД

---
+++ vue/src/components/DigitInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <input class="form-control"
+         type="text"
+         :value="value"
+         @focus="lastValid = $event.target.value"
+         @input="validate"
+         @blur="updateState">
+</template>
+
+<script>
+
+export default {
+  name: "DigitInput",
+  props: ['identity'],
+  data() {
+    return {
+      lastValid : null
+    }
+  },
+  methods: {
+    updateState(e) {
+      const newValue = e.target.value.trim()
+      if (newValue !== '') {
+        this.$store.dispatch('updateData', {identity: this.identity, value: e.target.value});
+      }
+    },
+    validate(e) {
+      const pattern = /^(0|[1-9]\d*)$/g;
+      if (!pattern.test(e.target.value)) {
+        e.target.value = this.lastValid;
+      } else {
+        this.lastValid = e.target.value
+      }
+    }
+  },
+  computed: {
+    value() {
+      return this.identity.reduce((acc, c) => acc && acc[c], this.$store.state)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>

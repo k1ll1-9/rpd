4a433b46219a37c0a3015621115dd9bbb86d0178
k1ll1 <blazehardtoerase@gmail.com>
2022-03-21 20:25:48 +0300

    семинары обновлены, добавлена отправка данных на генерацию pdf

---
+++ vue/src/components/Units/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-5">
+    <h3 class="my-4">Оглавление</h3>
+    <ul>
+      <li><a href="">6. Цель оценочные средства</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Navigation",
+  data() {
+    return {}
+  },
+  computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>

5a557e4343ecba49db61cf6481a4cdbb096f406a
k1ll1 <blazehardtoerase@gmail.com>
2022-03-25 17:41:02 +0300

    bootstrap селект заменен на syncfusion

--- vue/src/components/UI/Select.vue
+++ vue/src/components/UI/Select.vue
@@ -1,20 +1,21 @@
 <template>
-  <select class="form-select text-center" @change="updateState" >
-    <option v-if="value === null" disabled selected value></option>
-    <option v-for="(option,index) in options"
-            :selected="option.toString() === value"
-            :value="option"
-            :key="index">
-      {{ option }}
-    </option>
-  </select>
+  <DropDown :dataSource="options"
+            cssClass="defaults"
+            width="60%"
+            :value="value"
+            @change="updateState"/>
 </template>
 
 <script>
 
+import { DropDownListComponent } from "@syncfusion/ej2-vue-dropdowns";
+
 export default {
   name: "Select",
   props: ['identity', 'options'],
+  components : {
+    "DropDown" : DropDownListComponent
+  },
   data() {
     return {
       lastValid: null
@@ -24,7 +25,7 @@ export default {
     updateState(e) {
       this.$store.dispatch('updateData', {
         identity: this.identity,
-        value: e.target.value,
+        value: e.value,
         updateType: 'UPDATE_RPD_ITEM'
       });
     },
@@ -38,6 +39,32 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+@import "../../../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../../../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../../../node_modules/@syncfusion/ej2-vue-dropdowns/styles/material.css";
+
+.defaults.e-ddl.e-input-group.e-control-wrapper .e-input {
+ text-align: center;
+}
+.defaults .e-list-item{
+  color: #000000;
+}
+.defaults .e-dropdownbase .e-list-item:active {
+  color: #0b66c3;
+}
+.defaults.e-input-group:not(.e-float-icon-left):not(.e-float-input)::before,
+.defaults.e-input-group:not(.e-float-icon-left):not(.e-float-input)::after {
+  height: 2px;
+  background: #0b66c3;
+}
+.defaults .e-dropdownbase .e-list-item.e-active,
+.defaults .e-dropdownbase .e-list-item.e-active.e-hover {
+  color: #0b66c3;
+}
+.e-input-group:not(.e-float-icon-left):not(.e-float-input)::before, .e-input-group:not(.e-float-icon-left):not(.e-float-input)::after, .e-input-group.e-float-icon-left:not(.e-float-input) .e-input-in-wrap::before, .e-input-group.e-float-icon-left:not(.e-float-input) .e-input-in-wrap::after, .e-input-group.e-control-wrapper:not(.e-float-icon-left):not(.e-float-input)::before, .e-input-group.e-control-wrapper:not(.e-float-icon-left):not(.e-float-input)::after, .e-input-group.e-control-wrapper.e-float-icon-left:not(.e-float-input) .e-input-in-wrap::before, .e-input-group.e-control-wrapper.e-float-icon-left:not(.e-float-input) .e-input-in-wrap::after
+{
+  color: blueviolet !important;
+}
 
 </style>

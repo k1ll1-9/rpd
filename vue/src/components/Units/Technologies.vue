c597f5f5a1ed129977c918f0c24e3e7ec0b7e060
k1ll1 <blazehardtoerase@gmail.com>
2022-03-30 14:30:15 +0300

    доработан модуль описания оценочных средств, добавлен модуль образовательных технологий

---
+++ vue/src/components/Units/Technologies.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-5">
+    <h3 class="my-4" :id="$store.state.static.unitTitles[6].code">6. {{ $store.state.static.unitTitles[6].title }}</h3>
+    <VisualEditor :identity="['managed','disciplinePlace']"/>
+  </div>
+</template>
+
+<script>
+
+import VisualEditor from "../UI/VisualEditor";
+
+export default {
+  components: {VisualEditor},
+  name: "Technologies",
+  data() {
+    return {}
+  },
+  computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
